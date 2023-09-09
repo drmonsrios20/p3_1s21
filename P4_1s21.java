6203b16b6974e74a281a7ab15a771b9666665322
drmonsrios20 <144178868+drmonsrios20@users.noreply.github.com>
2023-09-08 20:34:13 -0600

    Add files via upload
    dominguez rios estefanny montserrat 
    practica 3y4
    08/09/23 1s21

---
+++ P4_1s21.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class P3_1s21{
+	public static void public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		String nombre;
+		System.out.println("Ingresa tu nombre");
+		nombre = teclado.nextLine();
+		String edad;
+		System.out.println("Ingresa tu edad");
+		edad = teclado.nextLine();
+		String correo;
+		System.out.println("Ingresa tu correo");
+		correo = teclado.nextLine();
+		String telefono;
+		System.out.println("Ingresa tu telefono");
+		telefono.out.nextLine();
+		System.out.println("Bienvenido" + nombre + ", a la clase");
+		System.out.println("Tu edad es" + edad);
+		System.out.println("Tu correo es" + correo);
+		System.out.println("Tu telefono es" + telefono);
+	}
+}
